8cf65ed1fc62d1935ab68470cd5305d9acebf7fd
Sengkea <sengkeasorm2022@gmail.com>
2025-05-26 00:57:50 +0700

    first commit

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,6 @@ use App\Http\Controllers\Auth\SocialAuthController;
 Route::get('/auth/google', [SocialAuthController::class, 'redirectToGoogle']);
 Route::get('/auth/google/callback', [SocialAuthController::class, 'handleGoogleCallback']);
 
-// Protected routes example
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
